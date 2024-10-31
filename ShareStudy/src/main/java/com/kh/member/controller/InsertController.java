ec4f992c27f672e58c04bdec346de465e18c5195
jery <jery@192.168.150.24>
2024-10-31 18:14:30 +0900

    민지

--- ShareStudy/src/main/java/com/kh/member/controller/InsertController.java
+++ ShareStudy/src/main/java/com/kh/member/controller/InsertController.java
@@ -1,8 +1,6 @@
 package com.kh.member.controller;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class MemberEnrollFrom
+ * Servlet implementation class InsertController
  */
-@WebServlet("/enrollForm.me")
-public class MemberEnrollFrom extends HttpServlet {
+@WebServlet("/insert.me")
+public class InsertController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MemberEnrollFrom() {
+    public InsertController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,9 +26,8 @@ public class MemberEnrollFrom extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		RequestDispatcher view = request.getRequestDispatcher("/views/member/enrollForm.jsp");
-		view.forward(request, response);
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
